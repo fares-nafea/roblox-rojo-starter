7371040728905034239dbddadc05f796f13052b5
Fares Ahmed <fares.nafe3.12@gamil.com>
2026-01-28 17:42:27 +0200

    Refactor LocalScript2.client.luau to enhance UI management by utilizing 'ClearAll'; update Script8.server.lua to improve clarity by changing part reference from 'Part' to 'MovedPart'.

---
+++ src/server/Script11.server.lua
@@ -0,0 +1,19 @@
+local KickedPlayers = {
+    7998496836
+}
+
+game.Players.PlayerAdded:Connect(function(player)
+
+    -- لو اللاعب متبند قبل كده
+    if table.find(KickedPlayers, player.UserId) then
+        player:Kick("You have been banned")
+        return
+    end
+
+    -- مثال: تطرده بعد 5 ثواني
+    task.wait(5)
+
+    table.insert(KickedPlayers, player.UserId)
+    player:Kick("You have been banned")
+
+end)
